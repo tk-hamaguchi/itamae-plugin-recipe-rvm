7581f7c5876a22427753ad3b1e132521157572a6
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2015-08-30 09:16:19 +0900

    Added gpg check.

--- lib/itamae/plugin/recipe/rvm.rb
+++ lib/itamae/plugin/recipe/rvm.rb
@@ -2,6 +2,7 @@
 execute 'install GPG-Key' do
   user 'root'
   command 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3'
+  not_if 'gpg --list-key | grep D39DC0E3'
 end
 
 package 'curl' do
